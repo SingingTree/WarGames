becaf59145be5d766ebb84eb2a4a5a64bd0f5132
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-09-14 18:42:36 +1200

    Add 1-3.py to first set, simple obfuscated

---
+++ set1/1-3.py
@@ -0,0 +1,17 @@
+I=NameError
+g=print
+v=input
+try:
+ W=raw_input
+ v=W
+except I:
+ pass
+o="5w0rd"
+A="f15h"
+g("Please enter the password")
+q=v()
+if(q==o+A):
+ g("Success, huzzah")
+else:
+ g("Not success, boo")
+# Created by pyminifier (https://github.com/liftoff/pyminifier
