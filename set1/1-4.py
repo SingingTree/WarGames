bf6737d8ddd8c0ff21a550da44d23b8ec39c70eb
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-09-16 12:56:04 +1200

    Add 1-4.py, simple hashing in set 1

---
+++ set1/1-4.py
@@ -0,0 +1,22 @@
+# This is a hack to make sure Python 2.x and 3.x behave the same
+# You can ignore this bit
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+# Real program starts here
+def hash_password(string):
+    return str(len(string) % 10)
+
+
+hashed_password = "6"
+
+print("Please enter the password")
+password_input = input()
+
+if(hash_password(password_input) == hashed_password):
+    print("Success, huzzah")
+else:
+    print("Not success, boo")
