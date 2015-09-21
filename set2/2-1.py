590be013b585db27cb7af80a1089b50c3f2de694
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-09-21 22:01:31 +1200

    Set2: get started

---
+++ set2/2-1.py
@@ -0,0 +1,21 @@
+# This is a hack to make sure Python 2.x and 3.x behave the same
+# You can ignore this bit
+try:
+    input = raw_input
+except NameError:
+    pass
+
+# Real program starts here
+def hash(number):
+    return number % 23
+
+
+password_hash = 17
+
+print("Please enter the password")
+password_input = int(input())
+
+if(hash(password_input) == password_hash):
+    print("Success, huzzah")
+else:
+    print("Not success, boo")
---
+++ set2/README.md
@@ -0,0 +1,3 @@
+# Set 1
+
+This set examines hashing in more depth. It's going to use examples closer to the real world, as well as discuss in more detail how hashing is applied.
